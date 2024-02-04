976151bd3b48168cde60419b4b1b0a84f01ed2c7
danielitogomez <daniel.gomez.amaran@gmail.com>
2024-02-04 19:19:23 +0100

    adding input to roadmap

--- roadmap_data.py
+++ roadmap_data.py
@@ -1,15 +1,27 @@
 roadmap = {
-        '1-Operating Systems': {
+        '1-Networking': {
+            'Fundamentals': [
+                {'name': 'OSI Model', 'url': 'https://es.wikipedia.org/wiki/Modelo_OSI'},
+                {'name': 'TCP/IP', 'url': 'https://en.wikipedia.org/wiki/Internet_protocol_suite'},
+            ],
+            'Some Protocols': [
+                {'name': 'SSH', 'url': 'https://en.wikipedia.org/wiki/Secure_Shell'},
+                {'name': 'HTTP', 'url': 'https://en.wikipedia.org/wiki/HTTP'},
+                {'name': 'HTTPS', 'url': 'https://en.wikipedia.org/wiki/HTTPS'},
+                {'name': 'DNS', 'url': 'https://kodekloud.com/courses/python-basics/'},
+            ],
+        },
+        '2-Operating Systems': {
             'Linux': [
                 {'name': 'Debian/Ubuntu', 'url': 'https://www.freecodecamp.org/news/introduction-to-linux/'},
-                {'name': 'RHEL/Derivatives', 'url': 'https://kodekloud.com/courses/red-hat-certified-system-administratorrhcsa-2/'},
+                {'name': 'RHEL', 'url': 'https://kodekloud.com/courses/red-hat-certified-system-administratorrhcsa-2/'},
             ],
             'Windows': [
                 {'name': 'Windows Server', 'url': 'https://www.udemy.com/course/windows-server-2019-administration-h/'},
                 {'name': 'Power Shell', 'url': 'https://learn.microsoft.com/en-us/training/modules/introduction-to-powershell/'},
             ],
         },
-        '2-Bash and Python': {
+        '3-Bash and Python': {
             'Bash': [
                 {'name': 'Bash Beginner', 'url': 'https://kodekloud.com/courses/shell-scripts-for-beginners/'},
                 {'name': 'Bash Advanced', 'url': 'https://kodekloud.com/courses/advanced-bash-scripting/'},
@@ -20,10 +32,21 @@ roadmap = {
                 {'name': 'Python 100 days', 'url': 'https://www.udemy.com/course/100-days-of-code/'},
             ],
         },
-        '3-Containers': {
+        '4-Containers': {
             'Docker': [
                 {'name': 'Docker Beginner', 'url': 'https://kodekloud.com/courses/docker-for-the-absolute-beginner/'},
                 {'name': 'Docker Advanced', 'url': 'https://kodekloud.com/courses/docker-certified-associate-exam-course/'},
+            ],
+        },
+        '5-Orchestration tools': {
+            'Kubernetes': [
+                {'name': 'Kubernetes Beginner', 'url': 'https://kodekloud.com/courses/kubernetes-for-the-absolute-beginners-hands-on/'},
+                {'name': 'Kubernetes the Hard Way', 'url': 'https://github.com/kelseyhightower/kubernetes-the-hard-way'},
+            ],
+        },
+        '6-Cloud Providers': {
+            'Kubernetes': [
+                {'name': 'AWS', 'url': 'https://aws.amazon.com/training/digital/'},
             ],
         },
     }
